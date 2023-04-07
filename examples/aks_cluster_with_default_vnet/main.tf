5fd5dcbb059a987b88cfed68c1244863a05a236b
ted-cdw <ted.spinks@cdw.com>
2023-04-07 08:30:52 -0400

    Add READMEs for every module

--- examples/aks_cluster_with_default_vnet/main.tf
+++ examples/aks_cluster_with_default_vnet/main.tf
@@ -1,13 +1,10 @@
-# Example 1: Standalone AKS module, which lets AKS auto-create the default VNet and Subnets
-# Default VNet: 10.224.0.0/12
-# Default subnet: 10.224.0.0/16
-
 module "azure_aks_default_vnet_example" {
   source                            = "../../modules/aks_cluster"
   cluster_name                      = "aks-example-1"
   location                          = var.location
   ssh_public_key                    = var.ssh_public_key
   user_node_pool_count              = 1
+  network_plugin                    = "azureoverlay"
   tags                              = var.tags
   sku_tier                          = "Free"
   app_gateway_enable                = true
@@ -15,4 +12,3 @@ module "azure_aks_default_vnet_example" {
   # azure_rbac_reader_group_object_ids = ["5390308c-2651-44e7-b10b-42887107a3c8"]
   maintenance_allowed_windows = [{ day = "Tuesday", hours = [9, 10] }, { day = "Thursday", hours = [9, 10] }]
 }
-
